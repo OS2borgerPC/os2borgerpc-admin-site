4fe640bea7f5a4406cffe81283c4a97751d80c87
Carsten Agger <carstena@magenta.dk>
2020-12-03 10:39:20 +0000

    Update installation guide.

--- admin_site/bibos_admin/settings.py
+++ admin_site/bibos_admin/settings.py
@@ -62,11 +62,12 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = env('TIME_ZONE')
+# Timezone/Language
+TIME_ZONE = 'CET'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = env('LANGUAGE_CODE')
+LANGUAGE_CODE = 'da'
 
 LOCALE_PATHS = [
     os.path.join(install_dir, 'locale')
