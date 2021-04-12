f72a9bd594bad22c2e9eecc8d29777cbd9dfce34
syre <thesyre@pm.me>
2021-04-12 13:14:24 +0200

    lint

--- admin_site/system/templatetags/custom_tags.py
+++ admin_site/system/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+
 register = template.Library()
 
 # Don't think it's being used currently, so uncommenting for now
@@ -10,9 +11,9 @@ register = template.Library()
 # Add CSS classes to tags, e.g. django generated forms.
 @register.filter
 def add_class(field, class_name):
-    return field.as_widget(attrs={
-        "class": " ".join((field.css_classes(), class_name))
-    })
+    return field.as_widget(
+        attrs={"class": " ".join((field.css_classes(), class_name))}
+    )
 
 
 @register.simple_tag
@@ -20,9 +21,11 @@ def css_class_current(current_url, match):
     if current_url.find(match) != -1:
         # Don't highlight Scripts when on security/scripts
         # Don't highlight Advarsler when on security/scripts
-        if match == "scripts" and \
-            current_url.find("security/scripts") != -1 or \
-            match == "/security/" and \
-            current_url.find("security/scripts") != -1:
-                return
+        if (
+            match == "scripts"
+            and current_url.find("security/scripts") != -1
+            or match == "/security/"
+            and current_url.find("security/scripts") != -1
+        ):
+            return
         return "active"
