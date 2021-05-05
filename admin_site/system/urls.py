2833c9fad8ce67e6b22568202fad5cd59b98b6d1
syre <thesyre@pm.me>
2021-05-05 13:49:42 +0200

    update documentation

--- admin_site/system/urls.py
+++ admin_site/system/urls.py
@@ -8,7 +8,7 @@ from .views import SiteDetailView, UserCreate, UserUpdate, SiteConfiguration
 from .views import ScriptList, ScriptUpdate, ScriptCreate, ScriptDelete
 from .views import ScriptRun, PCUpdate, JobRestarter
 from .views import ConfigurationEntryCreate, ConfigurationEntryUpdate
-from .views import ConfigurationEntryDelete, JobInfo, TechDocView, DocView
+from .views import ConfigurationEntryDelete, JobInfo, DocView
 from .views import PCDelete, JSONSiteSummary
 from .views import ImageVersionsView
 # SecurityProblem and SecurityEvent related views
@@ -142,8 +142,23 @@ urlpatterns = [
         RedirectView.as_view(
             url='/static/docs/OS2BorgerPC-sikkerhedsovervågning.pdf'
         )),
-    url(r'^documentation/tech/(?P<name>[\d\w\/]+)/', TechDocView.as_view(),
-        name='tech_doc'),
+    url(r'^documentation/tech/os2borgerpc-image',
+        RedirectView.as_view(
+            url='https://os2borgerpc-image.readthedocs.io/en/latest/'
+        )),
+    url(r'^documentation/tech/os2borgerpc-admin',
+        RedirectView.as_view(
+            url='https://github.com/OS2borgerPC/admin-site'
+        )),
+    url(r'^documentation/tech/os2borgerpc-server-image',
+        RedirectView.as_view(
+            url='https://os2borgerpc-server-image.readthedocs.io/en/latest/'
+        )),
+    url(r'^documentation/tech/os2borgerpc-client',
+        RedirectView.as_view(
+            url='https://os2borgerpc-client.readthedocs.io/en/latest/'
+        )),
+
     url(r'^documentation/(?P<name>[\d\w\/]+)/', DocView.as_view(), name='doc'),
     url(r'^documentation/', DocView.as_view(), name='doc_root'),
 
