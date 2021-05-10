5900e9d8a4519479f3950ec4e43349c97e037485
syre <thesyre@pm.me>
2021-05-10 16:56:47 +0200

    rearrange migrations

--- admin_site/system/migrations/0011_add_created_and_modified_fields.py
+++ admin_site/system/migrations/0011_add_created_and_modified_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0011_script_author'),
+        ('system', '0010_script_deleted'),
     ]
 
     operations = [
