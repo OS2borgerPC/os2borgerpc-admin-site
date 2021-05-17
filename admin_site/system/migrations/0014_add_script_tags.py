2766a1eb05cadca6c2cd05491fe385fd1ef53c89
syre <thesyre@pm.me>
2021-05-17 10:19:22 +0200

    rearrange migration

--- admin_site/system/migrations/0014_add_script_tags.py
+++ admin_site/system/migrations/0014_add_script_tags.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0012_script_author'),
+        ('system', '0013_script_author'),
     ]
 
     operations = [
