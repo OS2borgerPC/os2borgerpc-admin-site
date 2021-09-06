4bdd60d8955cc6dd41f925ba3af5ea920da5de66
Søren Howe Gersager <thesyre@pm.me>
2021-09-06 10:19:58 +0200

    more documentation fixes

--- admin_site/system/views.py
+++ admin_site/system/views.py
@@ -1831,9 +1831,9 @@ documentation_menu_items = [
     ('om_os2borgerpc_admin', 'Om OS2borgerPC-Admin'),
 
     ('', 'Teknisk dokumentation'),
-    ('tech/os2borgerpc-image', 'OS2borgerPC Desktop Image'),
+    ('tech/os2borgerpc-image', 'OS2borgerPC Image'),
     ('tech/os2borgerpc-admin', 'OS2borgerPC Admin Site'),
-    ('tech/os2borgerpc-server-image', 'OS2borgerPC Server Image'),
+    ('tech/os2borgerpc-server-image', 'OS2displayPC Image'),
     ('tech/os2borgerpc-client', 'OS2borgerPC Client'),
 
 ]
