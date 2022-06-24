0b70cdb0c4a7af8ae45de26dc454885ad2661027
Marcus Funch <mfm@magenta.dk>
2022-06-24 15:39:57 +0200

    gunicorn: use forwarded IP from traefik

--- docker/gunicorn-settings.py
+++ docker/gunicorn-settings.py
@@ -14,5 +14,8 @@ errorlog =  "/log/error.log"
 worker_tmp_dir = "/dev/shm"
 max_requests = 1000
 max_requests_jitter = 50
-
-
+# The IP of the traefik container - if not specified gunicorn
+# only trusts x forwarded for coming from 127.0.0.1
+forwarded_allow_ips = "127.0.0.1,172.18.0.2"
+# Default access log format except showing X forwarded for IP instead of host IP, as the host IP is just traefik's
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
