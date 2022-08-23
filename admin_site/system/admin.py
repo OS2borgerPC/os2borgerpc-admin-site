f72c7e464cd062a3890a4a9040a5650fc5ebbe17
Marcus Funch <mfm@magenta.dk>
2022-08-23 13:48:51 +0200

    admin: Fix batch search

--- admin_site/system/admin.py
+++ admin_site/system/admin.py
@@ -92,7 +92,7 @@ class BatchAdmin(admin.ModelAdmin):
     list_display = ("site", "name", "script")
     fields = ("site", "name", "script")
     list_filter = ("site",)
-    search_fields = ("name", "site", "script")
+    search_fields = ("name", "site__name", "script__name")
     inlines = [JobInline, BatchParameterInline]
 
 
