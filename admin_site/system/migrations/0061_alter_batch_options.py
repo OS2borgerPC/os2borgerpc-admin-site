0d79e49052d7d1a6eaf0fc14b8d3af5af9d7f768
Andreas Poulsen <ap@magenta-aps.dk>
2022-10-18 10:02:29 +0200

    Fix migrations

--- admin_site/system/migrations/0061_alter_batch_options.py
+++ admin_site/system/migrations/0061_alter_batch_options.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0061_auto_20221012_1407'),
+        ('system', '0060_alter_changelog_options'),
     ]
 
     operations = [
