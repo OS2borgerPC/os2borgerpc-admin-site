65ee7332781116711e8221e1dbd5753331f8eacf
Marcus Funch <mfm@magenta.dk>
2022-10-18 10:27:16 +0200

    Fix migrations

--- admin_site/system/migrations/0062_auto_20221012_1407.py
+++ admin_site/system/migrations/0062_auto_20221012_1407.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0060_alter_changelog_options'),
+        ('system', '0061_alter_batch_options'),
     ]
 
     operations = [
