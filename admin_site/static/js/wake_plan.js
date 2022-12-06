f9976de3f5cd2f6f913a599551df6e58278c5df8
Marcus Funch <mfm@magenta.dk>
2022-12-06 18:03:10 +0100

    Wake plan: Warn when navigating away after changes to the form. Should this be in custom.js for all pages?

--- admin_site/static/js/wake_plan.js
+++ admin_site/static/js/wake_plan.js
@@ -119,3 +119,51 @@ function ReturnToLastVisitedWakePlan() {
     sessionStorage.getItem(WAKE_PLAN_FROM_URL_KEY)
     )
 }
+
+
+// Credit: https://stackoverflow.com/a/57069660
+'use strict';
+  (() => {
+    const modified_inputs = new Set();
+    const defaultValue = 'defaultValue';
+    // store default values
+    addEventListener('beforeinput', evt => {
+      const target = evt.target;
+      if (!(defaultValue in target.dataset)) {
+        target.dataset[defaultValue] = ('' + (target.value || target.textContent)).trim();
+      }
+    });
+
+    // detect input modifications
+    addEventListener('input', evt => {
+      const target = evt.target;
+      let original = target.dataset[defaultValue];
+
+      let current = ('' + (target.value || target.textContent)).trim();
+
+      if (original !== current) {
+        if (!modified_inputs.has(target)) {
+          modified_inputs.add(target);
+        }
+      } else if (modified_inputs.has(target)) {
+        modified_inputs.delete(target);
+      }
+    });
+
+    addEventListener(
+      'saved',
+      function(e) {
+        modified_inputs.clear()
+      },
+      false
+    );
+
+    addEventListener('beforeunload', evt => {
+      if (modified_inputs.size) {
+        const unsaved_changes_warning = 'Changes you made may not be saved.';
+        evt.returnValue = unsaved_changes_warning;
+        return unsaved_changes_warning;
+      }
+    });
+
+  })();
