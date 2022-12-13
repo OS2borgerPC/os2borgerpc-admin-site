f48b5e9fd55978f9f8f175042c5e3072497b58de
Marcus Funch <mfm@magenta.dk>
2022-12-13 10:09:25 +0100

    Wake plan: Remove warning when there are unsaved changes for now

--- admin_site/static/js/wake_plan.js
+++ admin_site/static/js/wake_plan.js
@@ -90,50 +90,51 @@ sessionStorage.setItem(
 
 
 // Warn when trying to leave the page after changes have been made but not saved
-// Limitation: Doesn't currently detect changes in picklists!
+// Limitation: Doesn't currently detect changes in picklists! We also need to disable it for the buttons into wake change events
+// TODO: Consider doing this everywhere in the future, adding it to custom.js
 // Credit: https://stackoverflow.com/a/57069660
-'use strict';
-  (() => {
-    const modified_inputs = new Set();
-    const defaultValue = 'defaultValue';
-    // store default values
-    addEventListener('beforeinput', evt => {
-      const target = evt.target;
-      if (!(defaultValue in target.dataset)) {
-        target.dataset[defaultValue] = ('' + (target.value || target.textContent)).trim();
-      }
-    });
-
-    // detect input modifications
-    addEventListener('input', evt => {
-      const target = evt.target;
-      let original = target.dataset[defaultValue];
-
-      let current = ('' + (target.value || target.textContent)).trim();
-
-      if (original !== current) {
-        if (!modified_inputs.has(target)) {
-          modified_inputs.add(target);
-        }
-      } else if (modified_inputs.has(target)) {
-        modified_inputs.delete(target);
-      }
-    });
-
-    addEventListener(
-      'saved',
-      function(e) {
-        modified_inputs.clear()
-      },
-      false
-    );
-
-    addEventListener('beforeunload', evt => {
-      if (modified_inputs.size) {
-        const unsaved_changes_warning = 'Ændringer du har lavet er ikke blevet gemt.';
-        evt.returnValue = unsaved_changes_warning;
-        return unsaved_changes_warning;
-      }
-    });
-
-  })();
+// 'use strict';
+//   (() => {
+//     const modified_inputs = new Set();
+//     const defaultValue = 'defaultValue';
+//     // store default values
+//     addEventListener('beforeinput', evt => {
+//       const target = evt.target;
+//       if (!(defaultValue in target.dataset)) {
+//         target.dataset[defaultValue] = ('' + (target.value || target.textContent)).trim();
+//       }
+//     });
+//
+//     // detect input modifications
+//     addEventListener('input', evt => {
+//       const target = evt.target;
+//       let original = target.dataset[defaultValue];
+//
+//       let current = ('' + (target.value || target.textContent)).trim();
+//
+//       if (original !== current) {
+//         if (!modified_inputs.has(target)) {
+//           modified_inputs.add(target);
+//         }
+//       } else if (modified_inputs.has(target)) {
+//         modified_inputs.delete(target);
+//       }
+//     });
+//
+//     addEventListener(
+//       'saved',
+//       function(e) {
+//         modified_inputs.clear()
+//       },
+//       false
+//     );
+//
+//     addEventListener('beforeunload', evt => {
+//       if (modified_inputs.size) {
+//         const unsaved_changes_warning = 'Ændringer du har lavet er ikke blevet gemt.';
+//         evt.returnValue = unsaved_changes_warning;
+//         return unsaved_changes_warning;
+//       }
+//     });
+//
+//   })();
