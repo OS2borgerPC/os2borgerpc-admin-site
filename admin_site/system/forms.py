afe0e61b7d6a0a0ecad99bbc400b19666760cfc7
Marcus Funch <mfm@magenta.dk>
2023-04-13 15:41:26 +0200

    fixup! Django 4.0: ugettext -> gettext

--- admin_site/system/forms.py
+++ admin_site/system/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ValidationError
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from system.models import (
     ChangelogComment,
