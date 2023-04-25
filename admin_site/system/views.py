7db3add40bbdc2b6d62c7bb16f849afa8dd095e8
Andreas Poulsen <ap@magenta-aps.dk>
2023-04-25 16:36:24 +0200

    Adjust layout

--- admin_site/system/views.py
+++ admin_site/system/views.py
@@ -33,6 +33,7 @@ from two_factor.plugins.phonenumber.utils import get_available_phone_methods
 from django_otp.decorators import otp_required
 from django_otp import devices_for_user, user_has_device
 from django_otp.plugins.otp_static.models import StaticToken
+from django.forms import Form
 
 from account.models import (
     UserProfile,
@@ -90,7 +91,10 @@ def run_wake_plan_script(site, pcs, args, user, type="remove"):
         script = Script.objects.get(uid="wake_plan_remove")
     batch = script.run_on(site, pcs, *args, user=user)
 
-def otp_check(view=None, redirect_field_name='next', login_url=None, if_configured=False):
+
+def otp_check(
+    view=None, redirect_field_name="next", login_url=None, if_configured=False
+):
     """
     Modfied version of otp_required that redirects to site root if you do not have a device configured
     The normal version redirects to the login page, which results in a loop of logging in,
@@ -100,11 +104,16 @@ def otp_check(view=None, redirect_field_name='next', login_url=None, if_configur
         login_url = "/"
 
     def test(user):
-        return user.is_verified() or (if_configured and user.is_authenticated and not user_has_device(user))
+        return user.is_verified() or (
+            if_configured and user.is_authenticated and not user_has_device(user)
+        )
 
-    decorator = user_passes_test(test, login_url=login_url, redirect_field_name=redirect_field_name)
+    decorator = user_passes_test(
+        test, login_url=login_url, redirect_field_name=redirect_field_name
+    )
 
     return decorator if (view is None) else decorator(view)
+
 
 # Mixin class to require login
 class LoginRequiredMixin(View):
@@ -346,6 +355,8 @@ class TwoFactor(SiteView, SuperAdminOrThisSiteMixin, SiteMixin):
 
 
 class AdminTwoFactorDisable(otp_views.DisableView, SuperAdminOrThisSiteMixin):
+    form_class = Form
+
     def get_success_url(self):
         success_url = "/site/%s/users/%s/" % (
             self.kwargs["slug"],
@@ -355,12 +366,14 @@ class AdminTwoFactorDisable(otp_views.DisableView, SuperAdminOrThisSiteMixin):
 
     def get_context_data(self, **kwargs):
         site = get_object_or_404(Site, uid=self.kwargs["slug"])
-        context = {
-            "site": site,
-        }
+        context = {"site": site, "user": self.request.user, "form": Form}
         return context
 
     def dispatch(self, *args, **kwargs):
+        """This function has been overwritten to make it use get_success_url
+        and to redirect when the username does not match"""
+        # If the username in the url doesn't match request.user.username,
+        # redirect back to the main site
         if self.request.user.username != self.kwargs["username"]:
             return redirect("/")
         fn = otp_required(
@@ -369,6 +382,7 @@ class AdminTwoFactorDisable(otp_views.DisableView, SuperAdminOrThisSiteMixin):
         return fn(*args, **kwargs)
 
     def form_valid(self, form):
+        """This function has been overwritten to make it use get_success_url"""
         for device in devices_for_user(self.request.user):
             device.delete()
         return redirect(self.get_success_url())
@@ -387,12 +401,15 @@ class AdminTwoFactorSetup(otp_views.SetupView, SuperAdminOrThisSiteMixin):
         user = self.request.user
         site = get_object_or_404(Site, uid=self.kwargs["slug"])
         context["site"] = site
+        # url to redirect to when the user clicks cancel
         context["cancel_url"] = "/site/%s/users/%s/" % (site.uid, user.username)
         return context
 
     def get(self, request, *args, **kwargs):
         """
         Start the setup wizard. Redirect if already enabled.
+        This function has been overwritten in order to redirect
+        when the username does not match
         """
         # If the username in the url doesn't match request.user.username,
         # redirect back to the main site
@@ -405,6 +422,9 @@ class AdminTwoFactorSetup(otp_views.SetupView, SuperAdminOrThisSiteMixin):
     def done(self, form_list, **kwargs):
         """
         Finish the wizard. Save all forms and redirect.
+        This function has been overwritten to make it
+        use get_success_url in the final redirect.
+        All other lines are unchanged.
         """
         # Remove secret key used for QR code generation
         try:
@@ -459,6 +479,7 @@ class AdminTwoFactorSetupComplete(
             handler = self.http_method_not_allowed
         return handler(request, *args, **kwargs)
 
+
 @method_decorator(otp_check, name="dispatch")
 class AdminTwoFactorBackupTokens(otp_views.BackupTokensView, SuperAdminOrThisSiteMixin):
     def get_context_data(self, **kwargs):
@@ -485,12 +506,14 @@ class AdminTwoFactorBackupTokens(otp_views.BackupTokensView, SuperAdminOrThisSit
     def form_valid(self, form):
         """
         Delete existing backup codes and generate new ones.
+        This function has been overwritten in order to change success_url
         """
         device = self.get_device()
         device.token_set.all().delete()
         for n in range(self.number_of_tokens):
             device.token_set.create(token=StaticToken.random_token())
 
+        # Stay on this page after generating new backup tokens
         success_url = "/site/%s/admin-two-factor/%s/backup-tokens/" % (
             self.kwargs["slug"],
             self.kwargs["username"],
