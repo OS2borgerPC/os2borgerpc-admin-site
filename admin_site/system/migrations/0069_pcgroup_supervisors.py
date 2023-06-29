a7ed3591f0f7e1c01ee245fd15b9a58fe2dd4c14
Andreas Poulsen <ap@magenta-aps.dk>
2023-06-29 10:28:00 +0200

    Make supercronic run in development environment

--- admin_site/system/migrations/0069_pcgroup_supervisors.py
+++ admin_site/system/migrations/0069_pcgroup_supervisors.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("system", "0068_site_rerun_asc"),
