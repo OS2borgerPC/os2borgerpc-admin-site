de788a43e9145b4ab4dc692968200b7de0f6407f
Marcus Funch <mfm@magenta.dk>
2023-07-04 10:42:12 +0200

    EventRuleServer added to live alongside SecurityProblems, and mostly working

--- admin_site/system/migrations/0070_remove_pc_is_update_required.py
+++ admin_site/system/migrations/0070_remove_pc_is_update_required.py
@@ -5,7 +5,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("system", "0069_pcgroup_supervisors"),
