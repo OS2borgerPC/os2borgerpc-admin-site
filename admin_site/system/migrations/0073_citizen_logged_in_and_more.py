a317d26faf0da36bbc882552e30aa8834ae914fe
Marcus Funch <mfm@magenta.dk>
2023-09-28 17:04:08 +0200

    Add generic changes saved notification to security problems and event rule servers

--- admin_site/system/migrations/0073_citizen_logged_in_and_more.py
+++ admin_site/system/migrations/0073_citizen_logged_in_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("system", "0072_eventruleserver_alter_securityevent_problem_and_more"),
     ]
