6514ee86d7a38b6c118af1187f5b591a9db4b384
Andreas Poulsen <ap@magenta-aps.dk>
2024-06-13 08:53:20 +0200

    Fix migration and related translation

--- admin_site/system/migrations/0087_alter_site_uid.py
+++ admin_site/system/migrations/0087_alter_site_uid.py
@@ -6,7 +6,6 @@ import re
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("system", "0086_remove_featurepermission_sites_remove_site_country_and_more"),
     ]
@@ -16,8 +15,8 @@ class Migration(migrations.Migration):
             model_name="site",
             name="uid",
             field=models.CharField(
-                help_text="Must be unique. Valid characters are a-z, A-Z, 0-9 and dashes. We suggest names like <organisation> or <organisation-location> (without brackets)",
-                max_length=255,
+                help_text="Must be unique. Valid characters are a-z, A-Z, 0-9 and dashes, and the length must be between 2-40 characters. We suggest names like <organisation> or <organisation-location> (without brackets)",
+                max_length=40,
                 unique=True,
                 validators=[
                     django.core.validators.RegexValidator(
